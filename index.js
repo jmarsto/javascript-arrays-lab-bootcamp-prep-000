88776d3013289edd3b1d29bfee212f196f2899fe
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-14 04:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ function appendKitten(name) {
   return [...kittens, name]
 }
 
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
