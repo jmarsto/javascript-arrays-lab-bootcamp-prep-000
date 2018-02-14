66b70ae62c97d8a055ac7ff330028a6c5ced3241
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-14 04:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kitten = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.push("name")
+  kittens.push(name)
   return kittens
 }
